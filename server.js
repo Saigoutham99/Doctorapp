22d7f8db85bb5422ee5be6daa895c60b42a8c777
Saigoutham Lingam <saigouthamlingam@Saigouthams-MacBook-Pro-6.local>
2025-04-13 22:27:52 -0400

    Added new files or made changes

--- server.js
+++ server.js
@@ -4,6 +4,7 @@ const colors = require("colors");
 const morgan = require("morgan");
 const cors = require("cors");
 const connectDB = require("./config/db");
+// const path = require('path');
 
 // MongoDB Connection
 connectDB();
@@ -20,6 +21,13 @@ app.use(morgan("dev"));
 app.use("/api/v1/user", require("./routes/userRoutes"));
 app.use("/api/v1/admin", require("./routes/adminRoutes"));
 app.use("/api/v1/doctor", require("./routes/doctorRoutes"));
+
+// //Static files access
+// app.use(express.static(path.join(--dirname, './client/build')))
+
+// app.get('*', function(req,res){
+//   res.sendFile(path.join(__dirname,"./client/build/index.html"));
+// });
 
 // Server Port
 const PORT = process.env.PORT || 8080;
